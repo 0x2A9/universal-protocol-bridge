0d3e6bb939a8da19c7dfa00e7b9dbbfaed10d20f
Mykyta Pashchenko <gilsllendo@gmail.com>
2025-12-28 22:30:18 +0000

    UPB-3 [FEATURE]: Add UI component

---
+++ desktop/mainwindow.cpp
@@ -0,0 +1,213 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QDateTime>
+#include <QScrollBar>
+#include <QTextCursor>
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+    , settings("UPB", "UPB UI")
+{
+    ui->setupUi(this);
+
+    ensureDefaults();
+    loadUiFromSettings();
+
+    connect(ui->dsSave,  &QPushButton::clicked, this, &MainWindow::saveDevice);
+    connect(ui->brsSave, &QPushButton::clicked, this, &MainWindow::saveBaudRate);
+    connect(ui->dtsSend, &QPushButton::clicked, this, &MainWindow::saveDataBits);
+    connect(ui->spsSave, &QPushButton::clicked, this, &MainWindow::saveStopBits);
+
+    connect(ui->pbsEven, &QRadioButton::clicked, this, &MainWindow::saveParity);
+    connect(ui->pbsOdd,  &QRadioButton::clicked, this, &MainWindow::saveParity);
+    connect(ui->pbsNone, &QRadioButton::clicked, this, &MainWindow::saveParity);
+
+    connect(ui->cbTimestamp, &QCheckBox::toggled,
+            this, [this](bool v){ settings.setValue(KEY_TIMESTAMP, v); });
+
+    connect(ui->cbHexView, &QCheckBox::toggled,
+            this, [this](bool v){ settings.setValue(KEY_HEX, v); });
+
+    connect(&serial, &QSerialPort::readyRead,
+            this, &MainWindow::onSerialDataReady);
+
+    connect(ui->smSend, &QPushButton::clicked,
+            this, &MainWindow::onSendMessage);
+
+    reconnect();
+}
+
+MainWindow::~MainWindow()
+{
+    serial.close();
+    delete ui;
+}
+
+void MainWindow::ensureDefaults()
+{
+    if (!settings.contains(KEY_DEVICE))    settings.setValue(KEY_DEVICE, DEF_DEVICE);
+    if (!settings.contains(KEY_BAUDRATE))  settings.setValue(KEY_BAUDRATE, DEF_BAUDRATE);
+    if (!settings.contains(KEY_DATABITS))  settings.setValue(KEY_DATABITS, DEF_DATABITS);
+    if (!settings.contains(KEY_STOPBITS))  settings.setValue(KEY_STOPBITS, DEF_STOPBITS);
+    if (!settings.contains(KEY_PARITY))    settings.setValue(KEY_PARITY, PARITY_NONE);
+    if (!settings.contains(KEY_LINE_END))  settings.setValue(KEY_LINE_END, LINE_LF);
+
+    if (!settings.contains(KEY_TIMESTAMP)) settings.setValue(KEY_TIMESTAMP, true);
+    if (!settings.contains(KEY_HEX))       settings.setValue(KEY_HEX, false);
+}
+
+void MainWindow::loadUiFromSettings()
+{
+    ui->dsInput->setPlainText(settings.value(KEY_DEVICE).toString());
+
+    ui->brsInput->setPlainText(QString::number(settings.value(KEY_BAUDRATE).toInt()));
+    ui->dtsInput->setPlainText(QString::number(settings.value(KEY_DATABITS).toInt()));
+    ui->spsInput->setPlainText(QString::number(settings.value(KEY_STOPBITS).toInt()));
+
+    const QString parity = settings.value(KEY_PARITY).toString();
+    ui->pbsEven->setChecked(parity == PARITY_EVEN);
+    ui->pbsOdd ->setChecked(parity == PARITY_ODD);
+    ui->pbsNone->setChecked(parity == PARITY_NONE);
+
+    ui->cbTimestamp->setChecked(settings.value(KEY_TIMESTAMP).toBool());
+    ui->cbHexView->setChecked(settings.value(KEY_HEX).toBool());
+    ui->cbLineEnding->setCurrentText(settings.value(KEY_LINE_END).toString());
+}
+
+void MainWindow::saveDevice()
+{
+    const QString dev = ui->dsInput->toPlainText().trimmed();
+    if (dev != settings.value(KEY_DEVICE).toString()) {
+        settings.setValue(KEY_DEVICE, dev);
+        reconnect();
+    }
+}
+
+void MainWindow::saveBaudRate()
+{
+    settings.setValue(KEY_BAUDRATE, ui->brsInput->toPlainText().toInt());
+}
+
+void MainWindow::saveDataBits()
+{
+    settings.setValue(KEY_DATABITS, ui->dtsInput->toPlainText().toInt());
+}
+
+void MainWindow::saveStopBits()
+{
+    settings.setValue(KEY_STOPBITS, ui->spsInput->toPlainText().toInt());
+}
+
+void MainWindow::saveParity()
+{
+    if (ui->pbsEven->isChecked())
+        settings.setValue(KEY_PARITY, PARITY_EVEN);
+    else if (ui->pbsOdd->isChecked())
+        settings.setValue(KEY_PARITY, PARITY_ODD);
+    else
+        settings.setValue(KEY_PARITY, PARITY_NONE);
+}
+
+void MainWindow::reconnect()
+{
+    serial.close();
+
+    const QString dev = settings.value(KEY_DEVICE).toString();
+    if (dev.isEmpty())
+        return;
+
+    serial.setPortName(dev);
+    serial.open(QIODevice::ReadWrite);
+}
+
+void MainWindow::onSerialDataReady()
+{
+    rxBuffer.append(serial.readAll());
+
+    while (rxBuffer.contains('\n')) {
+        const int idx = rxBuffer.indexOf('\n');
+        QByteArray line = rxBuffer.left(idx);
+        rxBuffer.remove(0, idx + 1);
+
+        line = line.trimmed();
+        if (!line.isEmpty())
+            appendRx(line);
+    }
+}
+
+void MainWindow::appendMessage(const QByteArray &data,
+                               QLatin1StringView prefix,
+                               const QColor &color)
+{
+    QTextCursor cursor(ui->messages->document());
+    const bool atBottom =
+        ui->messages->verticalScrollBar()->value() ==
+        ui->messages->verticalScrollBar()->maximum();
+
+    cursor.movePosition(QTextCursor::End);
+
+    QTextCharFormat fmt;
+    fmt.setForeground(color);
+
+    QString text = settings.value(KEY_HEX).toBool()
+                       ? data.toHex(' ')
+                       : QString::fromUtf8(data);
+
+    text = text.trimmed();
+
+    if (settings.value(KEY_TIMESTAMP).toBool()) {
+        cursor.insertText(
+            QDateTime::currentDateTime().toString("[hh:mm:ss.zzz] "),
+            fmt);
+    }
+
+    cursor.insertText(QStringLiteral("[") + prefix + "] " + text + "\n", fmt);
+
+    if (atBottom)
+        ui->messages->verticalScrollBar()->setValue(
+            ui->messages->verticalScrollBar()->maximum());
+}
+
+
+
+void MainWindow::appendRx(const QByteArray &data)
+{
+    appendMessage(data, QLatin1StringView{"RX"}, Qt::darkGreen);
+}
+
+
+void MainWindow::onSendMessage()
+{
+    if (!serial.isOpen()) {
+        QMessageBox::warning(this, "UART", "Serial port not open");
+        return;
+    }
+
+    QByteArray data = ui->smInput->toPlainText().toUtf8();
+
+    const QString lineEnding = ui->cbLineEnding->currentText();
+    if (lineEnding == LINE_CRLF)
+        data.append("\r\n");
+    else
+        data.append("\n");
+
+    serial.write(data);
+    appendTx(data);
+    ui->smInput->clear();
+}
+
+void MainWindow::appendTx(const QByteArray &data)
+{
+    appendMessage(data, QLatin1StringView{"TX"}, Qt::magenta);
+}
+
+
+QSerialPort::Parity MainWindow::parityFromString(QStringView p) const
+{
+    if (p == PARITY_EVEN) return QSerialPort::EvenParity;
+    if (p == PARITY_ODD)  return QSerialPort::OddParity;
+    return QSerialPort::NoParity;
+}
---
+++ desktop/mainwindow.h
@@ -0,0 +1,74 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QSerialPort>
+#include <QSettings>
+#include <QList>
+#include <QStringView>
+#include <QLatin1StringView>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void saveDevice();
+    void saveBaudRate();
+    void saveDataBits();
+    void saveStopBits();
+    void saveParity();
+
+    void onSendMessage();
+    void onSerialDataReady();
+
+private:
+    inline static constexpr QLatin1StringView KEY_DEVICE    {"uart/device"};
+    inline static constexpr QLatin1StringView KEY_BAUDRATE  {"uart/baudrate"};
+    inline static constexpr QLatin1StringView KEY_DATABITS  {"uart/databits"};
+    inline static constexpr QLatin1StringView KEY_STOPBITS  {"uart/stopbits"};
+    inline static constexpr QLatin1StringView KEY_PARITY    {"uart/parity"};
+    inline static constexpr QLatin1StringView KEY_LINE_END  {"uart/lineEnding"};
+
+    inline static constexpr QLatin1StringView KEY_TIMESTAMP {"view/timestamp"};
+    inline static constexpr QLatin1StringView KEY_HEX       {"view/hex"};
+
+    inline static constexpr QLatin1StringView PARITY_NONE {"None"};
+    inline static constexpr QLatin1StringView PARITY_EVEN {"Even"};
+    inline static constexpr QLatin1StringView PARITY_ODD  {"Odd"};
+
+    inline static constexpr QLatin1StringView LINE_LF   {"LF"};
+    inline static constexpr QLatin1StringView LINE_CRLF {"CRLF"};
+
+    static constexpr int DEF_BAUDRATE = 9600;
+    static constexpr QSerialPort::DataBits DEF_DATABITS = QSerialPort::Data8;
+    static constexpr QSerialPort::StopBits DEF_STOPBITS = QSerialPort::OneStop;
+    inline static constexpr QLatin1StringView DEF_DEVICE {"/dev/ttyUSB0"};
+
+    void ensureDefaults();
+    void loadUiFromSettings();
+    void reconnect();
+
+    void appendMessage(const QByteArray &data,
+                       QLatin1StringView prefix,
+                       const QColor &color);
+    void appendRx(const QByteArray &data);
+    void appendTx(const QByteArray &data);
+    QSerialPort::Parity parityFromString(QStringView p) const;
+
+private:
+    Ui::MainWindow *ui;
+    QSerialPort serial;
+    QSettings settings;
+    QByteArray rxBuffer;
+};
+
+#endif // MAINWINDOW_H
